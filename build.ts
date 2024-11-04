0f507be9b95f1cb3f9f49fad125719f446efeab5
baguette <tony.lbvre@gmail.com>
2024-11-04 20:54:07 +0100

    big

--- build.ts
+++ build.ts
@@ -1,34 +1,33 @@
 
-// ESM
-import { Notebook } from "crossnote"
-import { copyFile, cp, mkdir, realpath, rename, rm, rmdir } from 'fs/promises';
+
+import { Notebook } from "crossnote";
+import { cp, mkdir, realpath, rename, rm } from 'fs/promises';
+import { basename, format } from 'path';
+
+const SOURCE_FILENAME = "satis-master-plan.md"
 
 async function main() {
-    const notebookPath = await realpath(".")
-    const notebook = await Notebook.init({
-      notebookPath: notebookPath,
-      config: {
-        printBackground: false,
-        enableScriptExecution: true, // <= For running code chunks.
-      },
-    });
+  const notebookPath = await realpath("./src")
+  const notebook = await Notebook.init({
+    notebookPath: notebookPath,
+    config: {
+      printBackground: false,
+      enableScriptExecution: true, // <= For running code chunks.
+    },
+  });
 
-    // Get the markdown engine for a specific note file in your notebook.
-    const engine = notebook.getNoteMarkdownEngine('satis-master-plan.md');
+  // Get the markdown engine for a specific note file in your notebook.
+  const engine = notebook.getNoteMarkdownEngine(SOURCE_FILENAME);
 
-    // html export
-    await engine.htmlExport({ offline: false, runAllCodeChunks: true });
-    
-    // chrome (puppeteer) export
-    //await engine.chromeExport({ fileType: 'pdf', runAllCodeChunks: true });
+  // html export
+  await engine.htmlExport({ offline: false, runAllCodeChunks: true });
 
-    await rm("dist", {recursive: true, force: true})
-    await mkdir("dist")
-    await rename("satis-master-plan.html", "dist/index.html")
-    await cp("static", "dist/static", {recursive: true})
-    //await rename("README.pdf", "dist/satis_master_plan.pdf")
+  const destFilename = format({ base: basename(SOURCE_FILENAME), ext: "html" })
 
-    return process.exit();
-
+  process.chdir("src")
+  await rm("dist", { recursive: true, force: true })
+  await mkdir("dist")
+  await rename(destFilename, "dist/index.html")
+  await cp("assets", "dist/assets", { recursive: true })
 }
-main()
+await main()
